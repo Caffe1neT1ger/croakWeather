3b9c39849e1f133ca46405cba9dd494abe7316f7
Croak-Croak <azamat.kalmykov07@gmail.com>
2023-11-29 16:48:15 +0300

    Discard redux async acitons

--- src/asyncActions/asyncWeather.ts
+++ src/asyncActions/asyncWeather.ts
@@ -1,28 +1,28 @@
-import { useSelector } from "react-redux";
-import { getLocationById } from "../http/weatherApi";
-import { AppDispatch, rootState } from "../store";
-import { setCurrentLocationAction } from "../store/currentWeatherReducer";
-import {
-  ILocationMini,
-  IMainState,
-  loadLocationListAction,
-} from "../store/mainReducer";
+// import { useSelector } from "react-redux";
+// import { getLocationById } from "../http/weatherApi";
+// import { AppDispatch, rootState } from "../store";
+// import { setCurrentLocationAction } from "../store/currentWeatherReducer";
+// import {
+//   ILocationMini,
+//   IMainState,
+//   loadLocationListAction,
+// } from "../store/mainReducer";
 
-export const fetchCurrentWeather = (locationId: number) => {
-  return (dispatch: AppDispatch) => {
-    getLocationById(locationId).then((data) =>
-      dispatch(setCurrentLocationAction(data))
-    );
-  };
-};
-// export const getLocationListFromLocalStorage = () => {
-//   return async (dispatch: AppDispatch) => {
-//     const locationList: ILocationMini[] = JSON.parse(
-//       localStorage.getItem("locationList") || "[]"
-//     );
-//     getLocationById(locationList[0].id).then((data) =>
+// export const fetchCurrentWeather = (locationId: number) => {
+//   return (dispatch: AppDispatch) => {
+//     getLocationById(locationId).then((data) =>
 //       dispatch(setCurrentLocationAction(data))
 //     );
-//     return dispatch(loadLocationListAction(locationList));
 //   };
 // };
+// // export const getLocationListFromLocalStorage = () => {
+// //   return async (dispatch: AppDispatch) => {
+// //     const locationList: ILocationMini[] = JSON.parse(
+// //       localStorage.getItem("locationList") || "[]"
+// //     );
+// //     getLocationById(locationList[0].id).then((data) =>
+// //       dispatch(setCurrentLocationAction(data))
+// //     );
+// //     return dispatch(loadLocationListAction(locationList));
+// //   };
+// // };
